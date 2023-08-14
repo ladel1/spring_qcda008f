319242fcb08dc26d9ba26c95179bbd3b43f4f63b
Adel LATIBI <alatibi@campus-eni.fr>
2023-08-14 13:39:26 +0200

    Valildation

--- demojpa/src/main/java/fr/eni/demojpa/bo/Client.java
+++ demojpa/src/main/java/fr/eni/demojpa/bo/Client.java
@@ -3,12 +3,16 @@ package fr.eni.demojpa.bo;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,13 +28,35 @@ public class Client implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Integer id;
 	@Column(length = 30)
+	@Length(
+			min=2,
+			max=30,
+			message="Le champs Prénom doit être entre 2 et 30 caractères"
+			)
 	private String firstname;
 	@Column(length = 30)
+	@Length(
+			min=2,
+			max=30,
+			message="Le champs Nom doit être entre 2 et 30 caractères"
+			)	
 	private String lastname;
 	@Column(length = 60)
+	@Length(
+			min=5,
+			max=60,
+			message="Le champs Adresse doit être entre 5 et 60 caractères"
+			)	
 	private String address;
+	//@Pattern(regex="", flag="", message="")
 	@Column(length = 10)
-	private String tel;	
+	@Length(
+			min=4,
+			max=10,
+			message="Le champs Téléphone doit être entre 4 et 10 caractères"
+			)	
+	private String tel;
+	@Past(message = "La date de naissance doit être dans le passé")	
 	private LocalDate bday;
 	
 }
--- demojpa/src/main/java/fr/eni/demojpa/ihm/ClientController.java
+++ demojpa/src/main/java/fr/eni/demojpa/ihm/ClientController.java
@@ -1,15 +1,23 @@
 package fr.eni.demojpa.ihm;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import fr.eni.demojpa.bll.ClientService;
 import fr.eni.demojpa.bo.Client;
+import jakarta.validation.Valid;
 
 @Controller
 public class ClientController {
 
+	@Autowired
+	private ClientService clientService;
+	
 	@GetMapping({"/"})
 	public String displayAddClient(Model model) {
 		model.addAttribute("client",new Client());
@@ -17,9 +25,19 @@ public class ClientController {
 	}
 	
 	@PostMapping({"/client/ajouter"})
-	public String addClient() {
-		return "";
+	public String addClient( 
+			@Valid @ModelAttribute Client client, 
+			BindingResult br 
+			) {
+		
+			if(br.hasErrors()) return "client/add";
+			
+			clientService.addClient(client);
+			return "redirect:/";
 		}
+	
+	
+	
 	public String detailsClient() {
 		return "";
 	}
