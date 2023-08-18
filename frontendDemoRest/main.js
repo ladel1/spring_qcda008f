2304cef5e47a982335a98a96cdc5fc3c872e852e
Adel LATIBI <alatibi@campus-eni.fr>
2023-08-18 16:32:39 +0200

    Frontend rest avec js

--- frontendDemoRest/main.js
+++ frontendDemoRest/main.js
@@ -1,37 +1,68 @@
 const displayNode = document.querySelector("#display");
 const formNode = document.querySelector("#form");
+// inputs 
+const titreNode = document.querySelector("#titre");
+const contenuNode = document.querySelector("#contenu");
+const auteurNode = document.querySelector("#auteur");
 let btnState = true;
 
 
 const ENDPOINT = "http://localhost:8080";
-fetch(ENDPOINT+"/articles")
+
+function reset(){
+    titreNode.value=""
+    contenuNode.value=""
+    auteurNode.value=""
+}
+
+function init(){
+    fetch(ENDPOINT+"/articles")
     .then((reponse)=> reponse.json())
     .then((articles)=>{
         const ulNode = document.createElement("ul");
+        displayNode.innerHTML="";
         displayNode.append(ulNode);
         
         for (const article of articles) {
             const liNode = document.createElement("li");
             liNode.innerText = `Titre: ${article.titre}, 
                                 Contenu: ${article.contenu}, 
-                                Auteur: ${article.auteur}`;
+                                Auteur: ${article.auteur}
+                                `;
+            const buttonNode = document.createElement("button");
+            buttonNode.setAttribute("value",article.id);
+            buttonNode.innerText="Supprimer";
+            buttonNode.onclick = supprimerArticle;
+            liNode.append(buttonNode);                                
             ulNode.append(liNode);                                
         }
 
-    })
+    })    
+}
 
+function supprimerArticle(event){
+    const id = event.target.value;
+    fetch(ENDPOINT+`/articles/${id}`,{method:"DELETE"})
+    .then((response)=>{
+        if(response.status==204){
+            event.target.parentNode.remove();
+            console.log("l'article est supprimé");
+        }
+    })
+}    
 
 function afficherForm(event){
-    if(btnState){
+    if(btnState){// afficher le formulaire
         displayNode.style = "display:none;";
         formNode.style = "display:block;";
         event.target.innerText="Afficher liste"
         btnState=false;
-    }else{
+    }else{ // afficher la liste articles
         displayNode.style = "display:block;";
         formNode.style = "display:none;";
         event.target.innerText="Ajouter un article"
         btnState=true;
+        init()
     }
 }
 
@@ -41,9 +72,7 @@ function afficherForm(event){
 
 function ajouterArticle(event){
 
-    const titreNode = document.querySelector("#titre");
-    const contenuNode = document.querySelector("#contenu");
-    const auteurNode = document.querySelector("#auteur");
+
 
     const article = {
         titre: titreNode.value,
@@ -61,8 +90,13 @@ function ajouterArticle(event){
     .then((response)=> response.json())
     .then((article)=>{
         if(article.id>0){
+            reset();
             console.log("Article a bien été ajouté");
         }
     })
 
 }
+
+
+// init liste article
+init()
